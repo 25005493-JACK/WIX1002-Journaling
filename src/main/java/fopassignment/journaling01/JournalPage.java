64163671a41f4dc9fdf3a4deca862de3503a2350
Cheng Ying Chen <chengyc25@gmail.com>
2025-11-04 23:48:16 +0800

    Display all journal dates and let user to select either to create or view journal

--- src/main/java/fopassignment/journaling01/JournalPage.java
+++ src/main/java/fopassignment/journaling01/JournalPage.java
@@ -2,6 +2,7 @@
 package fopassignment.journaling01;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Scanner;
 public class JournalPage {
     public static void Journal(){
         LocalDate myDate = LocalDate.now();
@@ -19,10 +20,39 @@ public class JournalPage {
         System.out.println("The time now is " + myTime);
 //TanWeiFengEnd
 //ChengYingChenStart
-
-
+        LocalDate jourDate = LocalDate.now();
+        LocalDate regisDate = LocalDate.of(2025, 11, 1); //will set it later to real registration date according to each user 
+        LocalDate rD = regisDate;
+        int day = 1;
+        System.out.println("*******Journal Dates*******");
+        while(rD.isBefore(jourDate.plusDays(1)))
+        {
+            System.out.println(day + ". " + rD);
+            rD = rD.plusDays(1);
+            day ++;
+        }
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Select date to view journal or create a journal for today.");
+        int UserC = s.nextInt();
+        while(true)
+        {
+            if(UserC >= 1 && UserC <= day)
+            {
+                break;
+            }
+            else
+            {
+                System.out.println("Invalid date. Please choose again.");
+                UserC = s.nextInt();
+            }
+        }
 
 //ChengYingChenEnd
     
     }
+
+
+    
 }
+
