8c360d93bc3f52942ce1b3e79e3b592df1673332
Tan Wei Feng <Tan Wei Feng@Lenovo-LOQ>
2025-11-19 11:28:27 +0800

    user info can load into sql aldy but i havent figure out how to generate random id for user without repeating. will do later. but sql database can works from my side

--- src/main/java/fopassignment/journaling01/WelcomingPage.java
+++ src/main/java/fopassignment/journaling01/WelcomingPage.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.sql.Connection;
 
 public class WelcomingPage extends User {
     public static boolean Login(){ //compare user input with data in csv/txt file
@@ -92,6 +93,17 @@ public class WelcomingPage extends User {
                 e.printStackTrace();
             }
             //save all variable to txt here
+            
+            try (Connection conn = DBConnection.getConnection()) {
+            System.out.println("Database connected successfully!");
+
+            UserDAO dao = new UserDAO();
+            boolean success = dao.saveUser(userId, name, email, pw);
+            System.out.println(success ? "User saved!" : "Failed to save user.");
+            } 
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         else {
             System.out.println("\nYour password and confirm password does not match. Please try again.");
