143f01edea578f32420873930f707af30a61ad0b
Tan Wei Feng <Tan Wei Feng@Lenovo-LOQ>
2026-01-05 00:35:48 +0800

    update comment

--- src/main/java/fopassignment/journaling01/WeatherRecording.java
+++ src/main/java/fopassignment/journaling01/WeatherRecording.java
@@ -8,15 +8,13 @@ public class WeatherRecording {
     
     public String getTodayWeather() {
         try {
-            // the url might need to change
+
             String url = "https://api.data.gov.my/weather/forecast/?contains=WP%20Kuala%20Lumpur@location__location_name&sort=date&limit=1";
             
-            // call GET request
             String json = api.get(url);
             
             String rawForecast = extractSummaryForecast(json);
-            
-            // extract summary_forecast
+
             return mapForecast(rawForecast);
             
         } catch (Exception e) {
@@ -77,16 +75,6 @@ public class WeatherRecording {
     }
     
     public String extractSummaryForecast(String json) {
-        
-//        json = json.trim();
-//        
-//        if (json.startsWith("[")) {
-//            json = json.substring(1);
-//        }
-//        
-//        if (json.endsWith("]")) {
-//            json = json.substring(0, json.length() - 1);
-//        }
         
         String key = "\"summary_forecast\":\"";
         int start = json.indexOf(key);
